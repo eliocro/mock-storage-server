11e4ea990f31ea6d41fac90a78062c07510b7d98
Élio Cró <elio.cro@gmail.com>
2024-10-01 17:45:43 +0100

    Update routes to match API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
 import auth from '@/middleware/auth';
-import user from './user';
-import students from './students';
+import login from './login';
+import users from './users';
 
 const router = Router();
 
-router.use(user);
-router.use('/students', auth, students);
+router.use(login);
+router.use('/users', auth, users);
 
 router.get('/', (_, res) => {
   res.send('Storage API Mock Server');
